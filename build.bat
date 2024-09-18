f928fb43039c1054dd8e832d216001ad67b4baf6
ru-wallace <105855934+ru-wallace@users.noreply.github.com>
2024-09-18 17:20:31 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-zig build -freference-trace
-::.\zig-out\bin\mapParser.exe  --input-www www --output output --index-wasm ./zig-out/bin/index.wasm --osm-data ./res/map.osm 
-copy ".\zig-out\bin\index.wasm" ".\output\index.wasm"
-C:\Users\xgb17134\.conda\envs\general\python.exe -m http.server --directory output 
